234f4cfdeea7ce649cbf9b8b031e7438341f027c
tanvirnpm <tanvirnpm@gmail.com>
2021-11-13 22:07:41 +0600

    confirm modal some edited

--- src/Pages/Dashboard/Admin/ManageAllOrders/ManageAllOrders.js
+++ src/Pages/Dashboard/Admin/ManageAllOrders/ManageAllOrders.js
@@ -4,23 +4,23 @@ import DashboardMenu from '../../Dashboard/DashboardMenu';
 
 const ManageAllOrders = () => {
     const [orders, setOrders] = useState([]);
-      const [isLoading, setIsLoading] = useState(true)
-    useEffect(()=>{
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
         fetch('https://intense-dawn-68409.herokuapp.com/get-all-orders')
-        .then(res=> res.json())
-        .then(data => setOrders(data))
-        .finally(()=> setIsLoading(false))
-    },[])
+            .then(res => res.json())
+            .then(data => setOrders(data))
+            .finally(() => setIsLoading(false))
+    }, [])
     // order status change to shipped
     const orderShipped = id => {
-        fetch(`https://intense-dawn-68409.herokuapp.com/order/shipped/${id}`,{
+        fetch(`https://intense-dawn-68409.herokuapp.com/order/shipped/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        .then(res=>res.json())
-        .then(result => console.log(result))
+            .then(res => res.json())
+            .then(result => console.log(result))
     }
     // console.log(orders)
     return (
@@ -42,15 +42,15 @@ const ManageAllOrders = () => {
                         </thead>
                         <tbody>
                             {
-                                orders.map((item,index)=> <tr key={index}>
-                                    <th>{index+1}</th>
+                                orders.map((item, index) => <tr key={index}>
+                                    <th>{index + 1}</th>
                                     <td>{item.name}, {item.email}, {item.contact}, {item.address}</td>
                                     <td>{item.car.make} {item.car.model} {item.car.fuelType}, {item.car.year}, {item.car.color}</td>
                                     <td>{item.car.price} /-</td>
                                     <td>{item.status}</td>
                                     <td>
                                         {item.status == 'Pending' && <button className="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#confirmModal">Delete</button>}
-                                        {item.status == 'Pending' && <button onClick={()=> orderShipped(item._id)} className="btn btn-info btn-sm ms-1">Shipped</button>}
+                                        {item.status == 'Pending' && <button onClick={() => orderShipped(item._id)} className="btn btn-info btn-sm ms-1">Shipped</button>}
                                     </td>
                                 </tr>)
                             }
@@ -66,11 +66,12 @@ const ManageAllOrders = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Toyota Allion</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Confirmation</h5>
+
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            ...
+                            <p>Do You Really Want to Delete This ?</p>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-info" data-bs-dismiss="modal">Cancel</button>
--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -73,11 +73,11 @@ const MyOrders = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Toyota Allion</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Confirmation</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            ...
+                            <p>Do You Really Want to Delete This ?</p>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-info" data-bs-dismiss="modal">Cancel</button>
