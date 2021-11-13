c4d3592819f619acfc4747a2bf2ca9418c4d4805
tanvirnpm <tanvirnpm@gmail.com>
2021-11-13 21:58:43 +0600

    stock list page added spinner

--- src/Pages/StockList/StockList.js
+++ src/Pages/StockList/StockList.js
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import Spinner from '../../FeedBack/Spinner';
 import Footer from '../Shared/Footer/Footer';
 import Navbar from '../Shared/Header/Navbar';
 import StockListCard from './StockListCard';
 
 const StockList = () => {
-    const [allProdutcs, setAllProducts] = useState([])
-    useEffect(()=>{
+    const [allProdutcs, setAllProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
         fetch('https://intense-dawn-68409.herokuapp.com/get-all-products')
-        .then(res=>res.json())
-        .then(data=>setAllProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setAllProducts(data))
+            .finally(()=> setIsLoading(false))
+    }, [])
     // console.log(allProdutcs)
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="container my-5">
                 <h1 className="text-center mb-4">Update Stock List</h1>
                 <div className="row g-3">
                     {
-                        allProdutcs.map((product, index)=> <StockListCard key={index} product={product}/>)
+                        isLoading && <Spinner />
+                    }
+                    {
+                        allProdutcs.map((product, index) => <StockListCard key={index} product={product} />)
                     }
                 </div>
             </div>
             <Footer />
-            
+
         </div>
     );
 };
